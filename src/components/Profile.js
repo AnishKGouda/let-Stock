cd27b2239b4080f7000aae01ad2f76e13a3086e2
2deadmen <kartikhegde.2002@gmail.com>
2022-12-14 07:14:42 +0530

    lk

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { json } from 'react-router-dom'
 
 const Profile = () => {
+
+let json
+useEffect(() => {
+  fetchuserdata()
+
+},[])
+
+const fetchuserdata=async()=>{
+  const response = await fetch(`https://let-stock.vercel.app/api/auth/getuser`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    } 
+  }
+  )
+   json=await  response.json()
+   console.log(json)
+
+} 
+  
   return (
     <>Profile
     <div className="container">pic</div>
-    <div className="container">name</div>
+    <div className="container">Name :{json}</div>
     <hr />
-    <div className="container">details</div>
+    <div className="container">account verified :{json}</div>
     </>
   )
 }
