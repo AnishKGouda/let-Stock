c5fc2ed2d3b80741b11fb0780cb5aa0f73c8ffb2
2deadmen <kartikhegde.2002@gmail.com>
2022-12-17 07:42:13 +0530

    final

--- backend/routes/stocks.js
+++ backend/routes/stocks.js
@@ -41,11 +41,11 @@ router.post('/addstock', fetchuser, async (req, res) => {
 
     router.post('/deletestock', fetchuser, async (req, res) => {
       try {
-   let stock=Stock.findOneAndRemove({title:req.body.name}).then(
+    await Stock.findOneAndRemove({title:req.body.name}).then(
        
         res.json({success:true}))
        
-       await stock.save()
+       
       } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
