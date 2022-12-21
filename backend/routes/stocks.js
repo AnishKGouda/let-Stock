204236bf67ca25188c707e7f256ad6fd708e28e5
2deadmen <kartikhegde.2002@gmail.com>
2022-12-21 23:56:53 +0530

    sd

--- backend/routes/stocks.js
+++ backend/routes/stocks.js
@@ -41,8 +41,8 @@ router.post('/addstock', fetchuser, async (req, res) => {
 
     router.delete('/deletestock/:id', async (req, res) => {
       try {
-    let del=await Stock.findById(req.params.id)
-        await del.delete()
+    let del=await Stock.findByIdAndDelete(req.params.id)
+  
         res.json({success:true}
     )
        
